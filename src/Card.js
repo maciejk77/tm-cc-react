59521b1f7365d7c2e25ecc2c4b6b4f9348451543
Maciej Kurek <maciejk77@gmail.com>
2018-07-18 13:04:18 +0100

    Minor, clean up

--- src/Card.js
+++ src/Card.js
@@ -23,12 +23,12 @@ class Card extends Component {
     const { name, status, apr, balance_mths, purchase_mths, credit_avail } = this.props.card;
     return (
       <div>
-        <div>{apr}</div>
-        <div>{balance_mths}</div>
-        <div>{credit_avail}</div>
-        <div>{name}</div>
-        <div>{purchase_mths}</div>
-        <div>{status}</div>
+        <div>APR: {apr}%</div>
+        <div>Balance: {balance_mths} months</div>
+        <div>Credit available: £{credit_avail}</div>
+        <div>Card name: {name}</div>
+        <div>Purchase: {purchase_mths} months</div>
+        <div>Status: {typeof(status) === "number" && '£'}{status}</div>
         { this.state.to_add 
           ? <button onClick={() => this.handle_add(credit_avail)}>ADD</button> 
           : <button onClick={() => this.handle_remove(credit_avail)}>REMOVE</button> 
